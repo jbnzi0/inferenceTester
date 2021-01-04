68563f84dea72654e2898614884f79603b299223
Joseph-Emmanuel <josephE.banzio@gmail.com>
2021-01-04 17:29:16 +0100

    JE x Beker : Configs

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-    app = express();
-const bodyParser = require('body-parser');
+    bodyParser = require('body-parser');
     inference = require('./routers/inference');
+    multer = require('multer');
+    app = express();
+    multerMid = multer({
+        storage: multer.memoryStorage(),
+        limits: {
+          fileSize: 5 * 1024 * 1024, //file no larger than 5mb
+        },
+    });
+    
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(multerMid.single('file'));
 app.use('/inference', inference);
+
+
 
 app.listen(8080, () => {
     console.log("Server listening on port 8080");
