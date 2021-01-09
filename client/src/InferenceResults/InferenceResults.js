b96683e7d57856522bcffb5e442e0c00ee0ed312
Teak <36111222+Teakt@users.noreply.github.com>
2021-01-09 17:33:40 +0100

    Slide show bar
    - No images added yet

--- client/src/InferenceResults/InferenceResults.js
+++ client/src/InferenceResults/InferenceResults.js
@@ -11,20 +11,46 @@ import Container from '@material-ui/core/Container';
 import AppBar from '@material-ui/core/AppBar';
 import logo from "../images/logo_bee.png";
 import styles from "../styles.module.css";
+import "./inferenceresults.css";
+import Carousel from 'react-elastic-carousel';
+import ReactDOM from "react-dom";
+
 
 
 import React, {Component} from "react";
 
 
+function importAll(r) {
+    return r.keys().map(r);
+  }
+  
+const images = importAll(require.context('./results', false, /\.(png|jpe?g|svg)$/));
+
+
 class InferenceResults extends Component {
+
+    state = {
+        items: [
+          {id: 1, title: 'item #1'},
+          {id: 2, title: 'item #2'},
+          {id: 3, title: 'item #3'},
+          {id: 4, title: 'item #4'},
+          {id: 5, title: 'item #5'}
+        ]
+      }
 
     render(){
         
-
+        const { items } = this.state;
 
         return(
-            <div>InferenceResults</div>
+            <div>
+            
+            <Carousel>
+                {items.map(item => <div key={item.id}>{item.title}</div>)}
+            </Carousel>
 
+        </div>
 
         )
 
